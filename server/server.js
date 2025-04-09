e381965a9b6db39ab619de6e70ba7d143ca32575
usama <itsusamairshad@gmail.com>
2025-04-09 09:48:01 -0700

    update

--- server/server.js
+++ server/server.js
@@ -12,12 +12,19 @@ const PORT = process.env.PORT || 4000
 const app = express()
 await connectDB()
 
+// Intialize middleware
+// console.log(process.env.MONGODB_URI);
+// console.log(process.env.CLERK_WEBHOOK_SECRET);
+// console.log(process.env.CLIPDROP_API);
+
 
 app.use(express.json())
 app.use(cors())
 
 // API Routes
 
+
+app.get('/',(req,res)=> res.send('API chl rhi hai'))
 app.use('/api/user',userRouter)
 app.use('/api/image', imageRouter)
 
